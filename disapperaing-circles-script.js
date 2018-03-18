e376df33bda29e78b8a32431eec74a4c8d9dfe12
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-03-19 01:32:19 +0530

    Added an external JS file

---
+++ disapperaing-circles-script.js
@@ -0,0 +1,17 @@
+document.getElementById("red-circle").onclick = function () {
+    document.getElementById("red-circle").style.display = "none";
+}
+        
+document.getElementById("blue-circle").onclick = function () {
+    document.getElementById("blue-circle").style.display = "none";
+}
+        
+document.getElementById("green-circle").onclick = function () {
+    document.getElementById("green-circle").style.display = "none";
+}
+        
+document.getElementById("reset").onclick = function() {
+    document.getElementById("red-circle").style.display = "block";
+    document.getElementById("blue-circle").style.display = "block";
+    document.getElementById("green-circle").style.display = "block";
+}
