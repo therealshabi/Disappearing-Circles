95f44eb6855b6b26aacdb2bc7460440506e81813
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-03-20 01:31:40 +0530

    Formatted the files

--- disapperaing-circles-script.js
+++ disapperaing-circles-script.js
@@ -1,16 +1,16 @@
 document.getElementById("red-circle").onclick = function () {
     document.getElementById("red-circle").style.display = "none";
 }
-        
+
 document.getElementById("blue-circle").onclick = function () {
     document.getElementById("blue-circle").style.display = "none";
 }
-        
+
 document.getElementById("green-circle").onclick = function () {
     document.getElementById("green-circle").style.display = "none";
 }
-        
-document.getElementById("reset").onclick = function() {
+
+document.getElementById("reset").onclick = function () {
     document.getElementById("red-circle").style.display = "block";
     document.getElementById("blue-circle").style.display = "block";
     document.getElementById("green-circle").style.display = "block";
